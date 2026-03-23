d1e3aee8124f15be7d645603d819843fc7c84a68
David-te23 <david.norberg@elev.ga.ntig.se>
2026-03-23 14:59:27 +0100

    Har gjort klart header, footer. De är nu fullt responsiva och fungerar perfekt.
    Har påbörjat arbetet med index, main sektionen dvs flödet med quacks.

--- includes/footer.php
+++ includes/footer.php
@@ -2,12 +2,27 @@
     </div> <!-- closes row -->
 </div> <!-- closes container -->
 
-<footer class="site-footer">
-    <p>&copy; <?= date('Y') ?> My Home Project. All rights reserved.</p>
-    <p>
-        <a href="https://github.com/yourusername" target="_blank">GitHub</a> |
-        <a href="mailto:you@example.com">Contact</a>
-    </p>
+<footer class="site-footer py-4 mt-auto">
+    <div class="container">
+        <div class="d-flex flex-column flex-md-row align-items-center justify-content-between">
+
+            <!-- vänster sida, logga och copyright -->
+            <div class="d-flex flex-column flex-md-row align-items-center mb-3 mb-md-0">
+                <a href="index.php">
+                    <img src="../public/images/QuackerLogo.svg" alt="Quacker Logo" class="header-img">
+                </a>
+                
+                <p class="m-0">&copy; <?= date('Y') ?> Quacker. All rights reserved.</p>
+            </div>
+
+            <!-- Höger sida, länkar -->
+            <nav class="footer-links d-flex flex-column flex-md-row align-items-center gap-3 gap-md-4">
+                <a href="#">About us</a>
+                <a href="#">Terms of Service</a>
+                <a href="#">Privacy Policy</a>
+            </nav>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -3,8 +3,27 @@ $pageTitle = "Home"; // <-- set dynamic page title
 require_once __DIR__ . '/../includes/header.php';
 ?>
 
-<h2>Feed</h2>
-<p>This is the homepage.</p>
+<div class="trending-section mb-4 p-3 bg-light rounded shadow-sm">
+    <div class="d-flex justify-content-center">
+        <div class="mb-4 p-3 bg-light">
+            <h5 class="fw-bold text-center">For you</h5>
+            <a href="#"><div class="trending-section-card mb-4 p-3 rounded shadow-sm">
+            <h5 class="fw-bold text-center">#Quack</h5>
+            
+        </div>
+        </a>
+    </div> 
+     <div class="d-flex">
+        <div class="mb-4 p-3 bg-light">
+            <h5 class="fw-bold text-center">Following</h5>
+            <a href="#"><div class="trending-section-card mb-4 p-3 rounded shadow-sm">
+            <h5 class="fw-bold text-center">#Quack</h5>
+            
+        </div>
+        </a>
+    </div>            
+                
+</div>
 
 <?php
 require_once __DIR__ . '/../includes/footer.php';
