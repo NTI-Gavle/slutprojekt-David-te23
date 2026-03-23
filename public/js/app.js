20f1ba3c57bc1c3f72fc7a1e6d4a7fa7c1ff6e56
David-te23 <david.norberg@elev.ga.ntig.se>
2026-03-23 09:46:53 +0100

    Fixat searchbar i header så att den nu animeras rätt och att innehållet på höger sida döljs på ett snyggt sätt.

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchBtn = document.getElementById('mobileSearchBtn');
     const closeSearchBtn = document.getElementById('closeSearchBtn');
     const header = document.getElementById('siteheader');
+    const mobileInput = document.getElementById('mobileInput');
 
     searchBtn.addEventListener('click', () => {
-        header.classList.toggle('search-active');
-    })
-})
+        header.classList.add ('search-active')
+        mobileInput.focus();
+    });
+
+    closeSearchBtn.addEventListener('click', () => {
+        header.classList.remove('search-active');
+    });
+});
